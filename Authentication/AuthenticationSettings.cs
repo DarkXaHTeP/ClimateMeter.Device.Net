40e178606989876319493d93da4f2e3c1e567ac4
Volodymyr Osmak <darkxahtep@gmail.com>
2017-12-02 16:19:13 +0200

    Added authentication settings and token provider

---
+++ Authentication/AuthenticationSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace ClimateMeter.Device.Net.Authentication
+{
+    public class AuthenticationSettings
+    {
+        public string Instance { get; set; }
+        public string TenantId { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string Resource { get; set; }
+    }
+}
---
+++ Authentication/AuthenticationTokenProvider.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace ClimateMeter.Device.Net.Authentication
+{
+    public class AuthenticationTokenProvider
+    {
+        private readonly ILogger<AuthenticationTokenProvider> _log;
+        private readonly AuthenticationSettings _settings;
+
+        private string _accessToken;
+        private DateTimeOffset _expiresOn;
+
+        public AuthenticationTokenProvider(IOptions<AuthenticationSettings> options, ILogger<AuthenticationTokenProvider> log)
+        {
+            _log = log;
+            _settings = options.Value;
+        }
+        
+        public string ObtainJwtToken()
+        {
+            return GetCachedToken() ?? GetNewToken();
+        }
+
+        private string GetCachedToken()
+        {
+            if (_accessToken != null && _expiresOn > DateTimeOffset.UtcNow)
+            {
+                return _accessToken;
+            }
+
+            return null;
+        }
+        
+        private string GetNewToken()
+        {
+            var credential = new ClientCredential(_settings.ClientId, _settings.ClientSecret);
+            
+            var authenticationContext = new AuthenticationContext($"{_settings.Instance}/{_settings.TenantId}", false);
+
+            AuthenticationResult result = authenticationContext
+                .AcquireTokenAsync(_settings.Resource, credential)
+                .GetAwaiter()
+                .GetResult();
+            
+            _log.LogInformation("Acquired new access token");
+
+            _accessToken = result.AccessToken;
+            _expiresOn = result.ExpiresOn.AddMinutes(-1);
+
+            return _accessToken;
+        }
+    }
+}
