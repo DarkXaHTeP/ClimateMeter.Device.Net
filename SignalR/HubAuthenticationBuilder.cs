fad0a6871cf594ce281eabb6a4fca3a178868076
Volodymyr Osmak <darkxahtep@gmail.com>
2017-12-02 16:28:35 +0200

    Added Hub Authentication

---
+++ SignalR/HubAuthenticationBuilder.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace ClimateMeter.Device.Net.SignalR
+{
+    public class HubAuthenticationBuilder: IHubAuthenticationBuilder
+    {
+        private readonly Func<string> _accessTokenProvider;
+        private readonly IHubConnectionBuilder _connectionBuilder;
+        private string _tokenQueryStringParameterName = "JWT_TOKEN";
+
+        public HubAuthenticationBuilder(Func<string> accessTokenProvider, IHubConnectionBuilder connectionBuilder)
+        {
+            _accessTokenProvider = accessTokenProvider;
+            _connectionBuilder = connectionBuilder;
+        }
+
+        public IHubAuthenticationBuilder WithTokenQueryStringParameterName(string queryStringParameterName)
+        {
+            if (String.IsNullOrEmpty(queryStringParameterName))
+            {
+                throw new ArgumentNullException(nameof(queryStringParameterName));
+            }
+
+            _tokenQueryStringParameterName = queryStringParameterName;
+
+            return this;
+        }
+
+        public IHubConnectionBuilder WithUrl(string url)
+        {
+            return WithUrl(new Uri(url));
+        }
+
+        public IHubConnectionBuilder WithUrl(Uri url)
+        {
+            return _connectionBuilder
+                .WithMessageHandler(new AuthorizationHttpMessageHandler(_accessTokenProvider))
+                .WithUrl(BuildConnectionUrl(url));
+        }
+
+        private Uri BuildConnectionUrl(Uri url)
+        {
+            var uriBuilder = new UriBuilder(url);
+            
+            var queryString = HttpUtility.ParseQueryString(uriBuilder.Query);
+            if (queryString[_tokenQueryStringParameterName] != null)
+            {
+                throw new ArgumentException($"Url should not contain Query String parameter named {_tokenQueryStringParameterName}", nameof(url));
+            }
+            
+            var token = _accessTokenProvider();
+            queryString[_tokenQueryStringParameterName] = token;
+
+            uriBuilder.Query = queryString.ToString();
+
+            return uriBuilder.Uri;
+        }
+    }
+}
---
+++ SignalR/IHubAuthenticationBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace ClimateMeter.Device.Net.SignalR
+{
+    public interface IHubAuthenticationBuilder
+    {
+        IHubAuthenticationBuilder WithTokenQueryStringParameterName(string queryStringParameterName);
+        IHubConnectionBuilder WithUrl(string url);
+        IHubConnectionBuilder WithUrl(Uri url);
+    }
+}
