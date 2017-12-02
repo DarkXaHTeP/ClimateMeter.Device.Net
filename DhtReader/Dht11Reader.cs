4864f77991e327b1c04a9674608d00c3b2ad5555
Volodymyr Osmak <darkxahtep@gmail.com>
2017-12-02 16:35:54 +0200

    Added delay and logging to sensor reader

--- DhtReader/Dht11Reader.cs
+++ DhtReader/Dht11Reader.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using Microsoft.Extensions.Logging;
 
 namespace ClimateMeter.Device.Net.DhtReader
 {
     public class Dht11Reader: IDhtReader
     {
+        private readonly ILogger<Dht11Reader> _log;
+
+        public Dht11Reader(ILogger<Dht11Reader> log)
+        {
+            _log = log;
+        }
+        
         public void Initialize(int pin)
         {
             if (Dht11Wrapper.init_dht11(pin) != 1)
@@ -45,10 +54,13 @@ namespace ClimateMeter.Device.Net.DhtReader
 
         private DhtData? ReadSingleResult()
         {
+            Thread.Sleep(800); // a short delay to avoid issues with sensor called too often
             int readResult = Dht11Wrapper.retry_read_dht11_data(300);
             
             if (readResult != 1)
             {
+                _log.LogWarning("Receiived bad result from Sensor");
+                
                 return null;
             }
             
