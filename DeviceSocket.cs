1deae4454ad7bfbe8263fde80e30c74a274eaa19
Volodymyr Osmak <darkxahtep@gmail.com>
2017-12-02 16:52:37 +0200

    Added DeviceSocket placeholder

---
+++ DeviceSocket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using ClimateMeter.Device.Net.Authentication;
+
+namespace ClimateMeter.Device.Net
+{
+    public class DeviceSocket
+    {
+        private readonly AuthenticationTokenProvider _tokenProvider;
+
+        public DeviceSocket(AuthenticationTokenProvider tokenProvider)
+        {
+            _tokenProvider = tokenProvider;
+        }
+        
+        public Task<Guid> Connect(string deviceName, string deviceDescription)
+        {
+            return Task.FromResult(Guid.Empty);
+        }
+
+        public Task AddSensorReading(Guid deviceId, float temperature, float humidity)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
